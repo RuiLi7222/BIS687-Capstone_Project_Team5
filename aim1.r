b57946e97d3c35914ae5546e76cdfec1fb0b1623
G-Despacito <zhangxinyi2105@gmail.com>
2023-04-25 23:57:08 -0400

    resolve code merge issue

--- aim1.r
+++ aim1.r
@@ -2,7 +2,7 @@
 library(dplyr)
 
 
-#----------------- Descriptive Statistics -------------------#
+#----------------- Data Cleaning -------------------#
 # import realted covariates
 physical <- read.csv("physical.csv")
 # remove useless columns X, redundant alcohol,ethic, leisure social activity, types of transport
@@ -13,26 +13,26 @@ physical$sex_f31_0_0 <- factor(physical$sex_f31_0_0,
                                labels = c(0,1))
 
 physical$ipaq_activity_group_f22032_0_0 <- ifelse(physical$ipaq_activity_group_f22032_0_0=="low",1,
-                                           ifelse(physical$ipaq_activity_group_f22032_0_0=="moderate",2,3))
+                                                  ifelse(physical$ipaq_activity_group_f22032_0_0=="moderate",2,3))
 
 physical$alcohol_intake_frequency_f1558_0_0 <- ifelse(physical$alcohol_intake_frequency_f1558_0_0=="Never",0,
-                                               ifelse(physical$alcohol_intake_frequency_f1558_0_0=="Special occasions only",1,
-                                               ifelse(physical$alcohol_intake_frequency_f1558_0_0=="One to three times a month",2,
-                                               ifelse(physical$alcohol_intake_frequency_f1558_0_0=="Once or twice a week",3,
-                                               ifelse(physical$alcohol_intake_frequency_f1558_0_0=="Three or four times a week",4,
-                                               ifelse(physical$alcohol_intake_frequency_f1558_0_0=="Daily or almost daily",5,6))))))
+                                                      ifelse(physical$alcohol_intake_frequency_f1558_0_0=="Special occasions only",1,
+                                                             ifelse(physical$alcohol_intake_frequency_f1558_0_0=="One to three times a month",2,
+                                                                    ifelse(physical$alcohol_intake_frequency_f1558_0_0=="Once or twice a week",3,
+                                                                           ifelse(physical$alcohol_intake_frequency_f1558_0_0=="Three or four times a week",4,
+                                                                                  ifelse(physical$alcohol_intake_frequency_f1558_0_0=="Daily or almost daily",5,6))))))
 
 physical$frequency_of_friendfamily_visits_f1031_0_0 <- ifelse(physical$frequency_of_friendfamily_visits_f1031_0_0=="Never or almost never"|
                                                                 physical$frequency_of_friendfamily_visits_f1031_0_0=="No friends/family outside household",0,
-                                                       ifelse(physical$frequency_of_friendfamily_visits_f1031_0_0=="Once every few months",1,
-                                                       ifelse(physical$frequency_of_friendfamily_visits_f1031_0_0=="About once a month",2,
-                                                       ifelse(physical$frequency_of_friendfamily_visits_f1031_0_0=="About once a week",3,
-                                                       ifelse(physical$frequency_of_friendfamily_visits_f1031_0_0=="2-4 times a week",4,
-                                                       ifelse(physical$frequency_of_friendfamily_visits_f1031_0_0=="Almost daily",5,6))))))
+                                                              ifelse(physical$frequency_of_friendfamily_visits_f1031_0_0=="Once every few months",1,
+                                                                     ifelse(physical$frequency_of_friendfamily_visits_f1031_0_0=="About once a month",2,
+                                                                            ifelse(physical$frequency_of_friendfamily_visits_f1031_0_0=="About once a week",3,
+                                                                                   ifelse(physical$frequency_of_friendfamily_visits_f1031_0_0=="2-4 times a week",4,
+                                                                                          ifelse(physical$frequency_of_friendfamily_visits_f1031_0_0=="Almost daily",5,6))))))
 
 physical$smoking_status_f20116_0_0 <- ifelse(physical$smoking_status_f20116_0_0=="Never",0,
-                                      ifelse(physical$smoking_status_f20116_0_0=="Previous",1,
-                                      ifelse(physical$smoking_status_f20116_0_0=="Current",2,NA)))
+                                             ifelse(physical$smoking_status_f20116_0_0=="Previous",1,
+                                                    ifelse(physical$smoking_status_f20116_0_0=="Current",2,NA)))
 physical$smoking_status_f20116_0_0 <- as.factor(physical$smoking_status_f20116_0_0)
 
 # negatives in sleep duration
@@ -49,8 +49,8 @@ physical$screen_time <- ifelse(physical$screen_time>=4, 1, 0)
 
 # import diet data
 ukbiobank <- readRDS("ukbiobank.rds")
-DQI_score <- read.csv("DQI_score.csv",skip = 1)
-DQI_score <- DQI_score[1:11270,-1]
+DQI_score <- read.csv("DQI_score.csv")
+DQI_score <- DQI_score[,-1]
 age <- ukbiobank[, c("eid", "age_at_recruitment_f21022_0_0")]
 
 df <- merge(physical, age, by = "eid")
@@ -62,7 +62,7 @@ df$DQI_score <- as.numeric(df$DQI_score)
 
 df <- na.omit(df)
 
-
+write.csv(df, "df.csv")
 
 #----------------- Descriptive Statistics -------------------#
 cat("\n\n#----------------- Descriptive Statistics -------------------#\n\n")
@@ -75,19 +75,34 @@ screen1 <- subset(df, screen_time == 1)
 screen_time_0_desc <- describe(screen0[c("age_at_recruitment_f21022_0_0",
                                          "total_met_minites_per_day",
                                          "DQI_score",
-                                         "total_screen_hours_per_day")])
+                                         "total_screen_hours_per_day",
+                                         "sleep_duration_f1160_0_0",
+                                         "alcohol_intake_frequency_f1558_0_0",
+                                         "standing_height_f50_0_0",
+                                         "weight_f21002_0_0",
+                                         "frequency_of_friendfamily_visits_f1031_0_0")])
 
 # calculate descriptive statistics for screen_time=1
 screen_time_1_desc <- describe(screen1[c("age_at_recruitment_f21022_0_0",
                                          "total_met_minites_per_day",
                                          "DQI_score",
-                                         "total_screen_hours_per_day")])
+                                         "total_screen_hours_per_day",
+                                         "sleep_duration_f1160_0_0",
+                                         "alcohol_intake_frequency_f1558_0_0",
+                                         "standing_height_f50_0_0",
+                                         "weight_f21002_0_0",
+                                         "frequency_of_friendfamily_visits_f1031_0_0")])
 
 
-# calculate percentage of females in each group
+# calculate percentage of females and smoke status in each group
 percent_female_0 <- round(prop.table(table(screen0$sex_f31_0_0))["0"] * 100, 2)
 percent_female_1 <- round(prop.table(table(screen1$sex_f31_0_0))["0"] * 100, 2)
-
+percent_never_smoke_0 <- round(prop.table(table(screen0$smoking_status_f20116_0_0))["0"] * 100, 2)
+percent_never_smoke_1 <- round(prop.table(table(screen1$smoking_status_f20116_0_0))["0"] * 100, 2)
+percent_previous_smoke_0 <- round(prop.table(table(screen0$smoking_status_f20116_0_0))["1"] * 100, 2)
+percent_previous_smoke_1 <- round(prop.table(table(screen1$smoking_status_f20116_0_0))["1"] * 100, 2)
+percent_current_smoke_0 <- round(prop.table(table(screen0$smoking_status_f20116_0_0))["2"] * 100, 2)
+percent_current_smoke_1 <- round(prop.table(table(screen1$smoking_status_f20116_0_0))["2"] * 100, 2)
 
 
 
@@ -95,7 +110,12 @@ percent_female_1 <- round(prop.table(table(screen1$sex_f31_0_0))["0"] * 100, 2)
 screen_time_desc <- describeBy(df[c("age_at_recruitment_f21022_0_0",
                                     "total_met_minites_per_day",
                                     "DQI_score",
-                                    "total_screen_hours_per_day")], 
+                                    "total_screen_hours_per_day",
+                                    "sleep_duration_f1160_0_0",
+                                    "alcohol_intake_frequency_f1558_0_0",
+                                    "standing_height_f50_0_0",
+                                    "weight_f21002_0_0",
+                                    "frequency_of_friendfamily_visits_f1031_0_0")], 
                                df$screen_time, 
                                mat=TRUE, 
                                digits=2, 
@@ -109,6 +129,12 @@ cat("Group 1: screen time > 4hr per day \n")
 screen_time_desc[, c(2, 4, 5, 6)] # display only mean and sd columns
 cat("Group 0: Percentage of females: ", percent_female_0, "% \n")
 cat("Group 1: Percentage of females: ", percent_female_1, "% \n")
+cat("Group 0: Percentage of never smoke: ", percent_never_smoke_0, "% \n")
+cat("Group 1: Percentage of never smoke: ", percent_never_smoke_1, "% \n")
+cat("Group 0: Percentage of previous smoke: ", percent_previous_smoke_0, "% \n")
+cat("Group 1: Percentage of previous smoke: ", percent_previous_smoke_1, "% \n")
+cat("Group 0: Percentage of current smoke: ", percent_current_smoke_0, "% \n")
+cat("Group 1: Percentage of current smoke: ", percent_current_smoke_1, "% \n")
 
 
 
@@ -133,25 +159,31 @@ wilcox.test(df$DQI_score[df$screen_time == 0], df$DQI_score[df$screen_time == 1]
 
 
 cat("\n\n#------------ Any DQI_score differences between sex=0,1? --------------#\n\n")
-# calculate the mean DQI score for each group
 mean_0 <- mean(df$DQI_score[df$sex_f31_0_0 == 0])
 mean_1 <- mean(df$DQI_score[df$sex_f31_0_0 == 1])
-
-# print the mean DQI score for each group
 cat("Mean DQI score for sex_f31_0_0 = 0:", mean_0, "\n")
 cat("Mean DQI score for sex_f31_0_0 = 1:", mean_1, "\n\n")
-
-# conduct Mann-Whitney U test and save the results to a local file
 wilcox.test(df$DQI_score[df$sex_f31_0_0 == 0], df$DQI_score[df$sex_f31_0_0 == 1], alternative = "two.sided")
+
+cat("\n\n#------------ Any DQI_score differences between smoking status=0,1,2? --------------#\n\n")
+mean_0 <- mean(df$DQI_score[df$smoking_status_f20116_0_0 == 0])
+mean_1 <- mean(df$DQI_score[df$smoking_status_f20116_0_0 == 1])
+mean_1 <- mean(df$DQI_score[df$smoking_status_f20116_0_0 == 2])
+cat("Mean DQI score for smoking_status_f20116_0_0 = 0:", mean_0, "\n")
+cat("Mean DQI score for smoking_status_f20116_0_0 = 1:", mean_1, "\n")
+cat("Mean DQI score for smoking_status_f20116_0_0 = 1:", mean_2, "\n\n")
+wilcox.test(df$DQI_score[df$smoking_status_f20116_0_0 == 0], df$DQI_score[df$smoking_status_f20116_0_0 == 1], alternative = "two.sided")
 
 
 #----------------- Linear Regression -------------------#
+cat("\n\n#----------------- Linear Regression -------------------#\n\n")
 # fit a multivariable linear regression model
 model <- lm(DQI_score ~ screen_time + sex_f31_0_0 + age_at_recruitment_f21022_0_0 + 
-              total_met_minites_per_day, data = df)
+              total_met_minites_per_day + sleep_duration_f1160_0_0 + alcohol_intake_frequency_f1558_0_0 +
+              standing_height_f50_0_0 + weight_f21002_0_0 + frequency_of_friendfamily_visits_f1031_0_0 + 
+              smoking_status_f20116_0_0, data = df)
 
 # display the regression 
-cat("\n\n#----------------- Linear Regression -------------------#\n\n")
 cat("Analysis Results\n")
 summary(model)
 sink()
@@ -185,15 +217,15 @@ fitY <- lm(DQI_score ~ ., data = dt)
 
 # Estimation via quasi-Bayesian approximation
 contcont <- mediation::mediate(fitM, fitY, data = dt,sims=50, 
-                    treat="screen_time", 
-                    mediator="total_met_minites_per_day")
+                               treat="screen_time", 
+                               mediator="total_met_minites_per_day")
 summary(contcont)
 plot(contcont)
 
 # Estimation via nonparametric bootstrap
 contcont.boot <- mediation::mediate(fitM, fitY, boot=TRUE, sims=50, 
-                         treat="screen_time", 
-                         mediator="total_met_minites_per_day")
+                                    treat="screen_time", 
+                                    mediator="total_met_minites_per_day")
 summary(contcont.boot)
 
 # Allowing treatment-mediator interaction
@@ -213,14 +245,14 @@ d.int <- lm(DQI_score ~ screen_time*sex_f31_0_0*total_met_minites_per_day+.,
             data = dt)
 
 contcont.female <- mediation::mediate(b.int, d.int, sims=50, 
-                           treat="screen_time", 
-                           mediator="total_met_minites_per_day",
-                           covariates = list(sex_f31_0_0 = 0))
+                                      treat="screen_time", 
+                                      mediator="total_met_minites_per_day",
+                                      covariates = list(sex_f31_0_0 = 0))
 
 contcont.male <- mediation::mediate(b.int, d.int, sims=50, 
-                         treat="screen_time", 
-                         mediator="total_met_minites_per_day",
-                         covariates = list(sex_f31_0_0 = 1))
+                                    treat="screen_time", 
+                                    mediator="total_met_minites_per_day",
+                                    covariates = list(sex_f31_0_0 = 1))
 
 summary(contcont.female)
 summary(contcont.male)
@@ -250,10 +282,6 @@ contcont.currentsmoking <- mediation::mediate(e.int, f.int, sims=50,
 summary(contcont.nosmoking)
 summary(contcont.previoussmoking)
 summary(contcont.currentsmoking)
-
-
-
-
 
 
 
